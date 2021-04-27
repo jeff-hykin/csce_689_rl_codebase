4eb31821446df9abd58a3a22d7a0bda0619b8818
Jeff Hykin <jeff.hykin@gmail.com>
2021-04-26 21:23:42 -0500

    add full documentation

--- Solvers/_1_Value_Iteration.py
+++ Solvers/_1_Value_Iteration.py
@@ -9,22 +9,6 @@ import numpy as np
 import heapq
 from Solvers.Abstract_Solver import AbstractSolver, Statistics
 
-
-
-import gym
-env = gym.make("CartPole-v1")
-observation = env.reset()
-for _ in range(1000):
-    env.render()
-    action = env.action_space.sample() # your agent here (this takes random actions)
-    observation, reward, done, info = env.step(action)
-
-    if done:
-        observation = env.reset()
-
-env.close()
-
-
 class ValueIteration(AbstractSolver):
 
     def __init__(self,env,options):
@@ -36,41 +20,67 @@ class ValueIteration(AbstractSolver):
 
     def train_episode(self):
         """
-            Run a single episode of the Value Iteration Algorithm.
-
-            Available inputs:
+            What/Where is this function?
+                Imagine the for loop from the gym intro (_0_Gym_Introduction.py)
+                This function effectively gets called 1 time for each
+                loop/iteration of that for loop.
+                It is just in a class with more variables and logging
+            
+            Inputs: (Available/Useful variables)
+                self.env
+                    this is no different than 
+                        import gym
+                        self.env = gym.make("CartPole-v1")
+                        # from http://gym.openai.com/
+                    except that its easier to switch environments
+                
                 state = self.env.reset():
                     returns a state which is just an integer index
                     (not all problems have state as an index, but this one does)
+                    more specifically it resets the environment and returns the stating state
                 
-                state = self.env.reset():
-                
-                self.env:
-                    OpenAI env. env.P represents the transition probabilities of the environment.
-                
-                self.env:
-                    OpenAI environment.
+                self.env.nS:
+                    number of states in the environment
                     
-                    env.P:
-                        represents the transition probabilities of the environment
-                    env.P[s][a] is a list of transition tuples (prob, next_state, reward, done).
-                    env.nS is a number of states in the environment.
-                    env.nA is a number of actions in the environment.
-                self.options.gamma: Gamma discount factor.
+                self.env.nA:
+                    number of actions in the environment
+                    
+                for probability, next_state, reward, done in self.P[state][action]:
+                    `probability` will be probability of `next_state` actually being the next state
+                    `reward` is the short-term/immediate reward for achieving that next state
+                    `done` is a boolean of wether or not that next state is the last/terminal state
+                    
+                    Every action has a chance (at least theortically) of different outcomes (states)
+                    Which is why `self.P[state][action]` is a list of outcomes and not a single outcome
+                
+                self.options.gamma:
+                    The discount factor (gamma from the slides)
+            
+            Outputs: (what you need to output/update)
+                self.V:
+                    This should be a dictionary
+                    `self.V[state]` should return a floating point value that
+                    represents the value of a state. This value should become 
+                    more accurate with each episode.
+                    
+                    How should this be calculated?
+                        look at the value iteration algorithm
+                        Ref: Sutton book eq. 4.10.
+                    Once those values have been updated, thats it for this function/class
         """
+        
+        # you can add variables here if it is helpful
+        
+        # Update the estimated value of each state
+        for each_state in range(self.env.nS):
+            
+            ###################################################
+            #            Compute self.V here                  #
+            # Do a one-step lookahead to find the best action #
+            ###################################################
 
-        # Update each state...
-        for s in range(self.env.nS):
-            # Do a one-step lookahead to find the best action
-            # Update the value function. Ref: Sutton book eq. 4.10.
 
-            ################################
-            #   YOUR IMPLEMENTATION HERE   #
-            ################################
-
-
-        # In DP methods we don't interact with the environment so we will set the reward to be the sum of state values
-        # and the number of steps to -1 representing an invalid value
+        # Dont worry about this part
         self.statistics[Statistics.Rewards.value] = np.sum(self.V)
         self.statistics[Statistics.Steps.value] = -1
 
@@ -90,11 +100,47 @@ class ValueIteration(AbstractSolver):
         """
 
         def policy_fn(state):
+            """
+            What/Where is this function?
+                Imagine the gym intro code (_0_Gym_Introduction.py)
+                This function is the part that decides what action to take
+            
+            Inputs: (Available/Useful variables)
+                self.V[state]
+                    the estimated long-term value of getting to a state
+                
+                values = self.one_step_lookahead(state)
+                    len(values) will be the number of actions (self.env.nA)
+                    values[action] will be the expected value of that action (float)
+                    
+                for probability, next_state, reward, done in self.P[state][action]:
+                    `probability` will be probability of `next_state` actually being the next state
+                    `reward` is the short-term/immediate reward for achieving that next state
+                    `done` is a boolean of wether or not that next state is the last/terminal state
+                    
+                    Every action has a chance (at least theortically) of different outcomes (states)
+                    Which is why `self.P[state][action]` is a list of outcomes and not a single outcome
+                
+                self.env.nS:
+                    number of states in the environment
+                    
+                self.env.nA:
+                    number of actions in the environment
+                    
+                self.options.gamma:
+                    The discount factor (gamma from the slides)
+            
+            Outputs: (what you need to output)
+                return a list
+                    len(the_list) should be the number of actions
+                    the action we want to take should have a value of 1
+                    all other actions should have a value of 0
+            """
+            
             ################################
             #   YOUR IMPLEMENTATION HERE   #
             ################################
-
-
+        
         return policy_fn
 
 
@@ -153,7 +199,7 @@ class AsynchVI(ValueIteration):
     def __str__(self):
         return "Asynchronous VI"
 
-    def one_step_lookahead(self,state):
+    def one_step_lookahead(self, state: int):
         """
         Helper function to calculate the value for all action in a given state.
 
